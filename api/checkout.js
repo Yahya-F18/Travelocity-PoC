537c9926f4ff17a47481ad0d2bf838d927c950b4
Yahya-F18 <sydalbna907@gmail.com>
2026-09-02 09:55:57 +0300

    fix

--- api/checkout.js
+++ api/checkout.js
@@ -2,16 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = (req, res) => {
-  // قراءة الهيدرز الواردة في الطلب الأول
-  const initialHeaders = JSON.stringify(req.headers, null, 2);
-  
-  // قراءة ملف الـ HTML الأصلي
-  let html = fs.readFileSync(path.join(process.cwd(), 'index.html'), 'utf8');
-  
-  // حقن الهيدرز في كائن جافاسكريبت داخل الصفحة
-  const injectedScript = `<script>window.__INITIAL_HEADERS__ = ${initialHeaders};</script>`;
-  html = html.replace('<head>', '<head>' + injectedScript);
-
-  res.setHeader('content-type', 'text/html; charset=utf-8');
-  res.status(200).send(html);
+  try {
+    const initialHeaders = JSON.stringify(req.headers, null, 2);
+    let html = fs.readFileSync(path.join(__dirname, '_page.html'), 'utf8');
+    const injectedScript = `<script>window.__INITIAL_HEADERS__ = ${initialHeaders};</script>`;
+    html = html.replace('<head>', '<head>' + injectedScript);
+    res.setHeader('content-type', 'text/html; charset=utf-8');
+    res.status(200).send(html);
+  } catch (e) {
+    res.status(500).send('error: ' + e.message);
+  }
 };
